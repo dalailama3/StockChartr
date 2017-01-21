5a367d8b5a647c31651950ef29047d9a0ccd154c
John Clark <johngclark36@gmail.com>
2017-01-21 16:38:27 -0500

    Setup project file structure, npm install express and mongoose

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express()
+
+// app.set('views', process.cwd() + '/views')
+
+// app.set('view engine', 'html')
+
+
+app.use(express.static(__dirname + '/app/public'))
+var routes = require('./app/routes/index.js')
+
+
+
+routes(app)
+
+
+app.listen(process.env.PORT || '8080')
